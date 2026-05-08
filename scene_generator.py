c0faa4aaef33fa2f720706c2259f831cad36a1f9
Saikumar223 <75354263+Saikumar223@users.noreply.github.com>
2026-05-08 22:11:17 +0530

    Update scene_generator.py

--- scene_generator.py
+++ scene_generator.py
@@ -1,1 +1,33 @@
+def generate_scenes(parsed_data):
 
+    theme = parsed_data["theme"]
+
+    if theme == "EAMCET preparation":
+
+        scenes = [
+            {
+                "scene_number": 1,
+                "description": "Keerthi studying physics with messy notes and tired expression"
+            },
+            {
+                "scene_number": 2,
+                "description": "Keerthi taking mobile during study break"
+            },
+            {
+                "scene_number": 3,
+                "description": "Keerthi drinking chai near rainy window while relaxing"
+            },
+            {
+                "scene_number": 4,
+                "description": "Keerthi realizing too much break time passed and returning to study"
+            }
+        ]
+
+        return scenes
+
+    return [
+        {
+            "scene_number": 1,
+            "description": "Keerthi casually sitting in her room"
+        }
+    ]
