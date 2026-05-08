ac9f7f85a2771fb2e83ee482ca934b915b362bbc
Saikumar223 <75354263+Saikumar223@users.noreply.github.com>
2026-05-08 22:12:30 +0530

    Update caption_generator.py

--- caption_generator.py
+++ caption_generator.py
@@ -1,1 +1,11 @@
+def generate_caption(parsed_data):
 
+    theme = parsed_data["theme"]
+
+    if theme == "EAMCET preparation":
+        return "5 mins break ani reel chestunna 😭"
+
+    if theme == "exam stress":
+        return "Survival mode activated 💀"
+
+    return "Just another student day ✨"
